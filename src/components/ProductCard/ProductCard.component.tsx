fc1c41f15349d28bc0c035397359f4b8ba342443
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2026-04-17 17:40:28 +0530

    fix all issues component

--- src/components/ProductCard/ProductCard.component.tsx
+++ src/components/ProductCard/ProductCard.component.tsx
@@ -11,17 +11,17 @@ const ProductCard: React.FC<ProductCardProps> = (props) => {
       onClick={onClick}
       className="flex gap-4 justify-between w-xs min-h-100"
     >
-      <div className=" flex flex-col text-center shadow-lg rounded-sm overflow-hidden">
+      <div className="flex flex-col text-center shadow-lg rounded-sm overflow-hidden">
         <div className="max-h-80 min-h-72 border-b border-lightGray">
-          <img src={image} alt={title} className="w-full h-full object-cover" />
+          <img src={image} alt={title} className="w-full h-full object-cover"/>
         </div>
 
         <div className="mt-2 mb-3 text-center px-2">
-          <Typography variant="headline" text={title} weight="medium" />
+          <Typography variant="headline" text={title} weight="medium"/>
         </div>
 
-        <div className="mt-auto px-2 mb-3">
-          <Button onClick={onClick} text="VIEW DETAILS" iconName="arrowUp" />
+        <div className="mt-auto mb-3 w-62 mx-auto">
+          <Button onClick={onClick} text="VIEW DETAILS" iconName="arrowUp" size="full"/>
         </div>
       </div>
     </div>
