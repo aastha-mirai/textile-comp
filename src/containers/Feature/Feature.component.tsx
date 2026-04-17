2392529b1fc71d0a62d8babe22866bc76692fe59
Durga <durga.jaiswal@miraiedge.com>
2026-04-17 19:18:29 +0530

    add gap

--- src/containers/Feature/Feature.component.tsx
+++ src/containers/Feature/Feature.component.tsx
@@ -5,7 +5,7 @@ const Feature = () => {
   return (
     <section
       aria-label="Stats Section"
-      className="h-auto flex bg-theme-gradient items-center justify-center gap-2 px-1 xl:gap-0 py-6 flex-wrap"
+      className="h-auto flex bg-theme-gradient items-center justify-center gap-4 px-1 xl:gap-0 py-6 flex-wrap"
     >
       {featureStatsCard.map((card, index) => (
         <div key={card.id} className="flex items-center">
