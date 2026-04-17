76075e4c0a3687448f539fe8fcb21bd86c9e7097
Durga <durga.jaiswal@miraiedge.com>
2026-04-17 16:04:50 +0530

    Fix why Choose us section

---
+++ src/mockData/whyChooseUsStat.ts
@@ -0,0 +1,24 @@
+import type { FeatureStatsCardData } from "@components";
+
+export const whyChooseUsStat: FeatureStatsCardData[] = [
+  {
+    id: 1,
+    icon: "quality",
+    text: "10+ Years Experience",
+  },
+  {
+    id: 2,
+    icon: "construction",
+    text: "Skilled Fabrication Team",
+  },
+  {
+    id: 3,
+    icon: "industry",
+    text: "Modern Factory Setup",
+  },
+  {
+    id: 4,
+    icon: "meeting",
+    text: "Tender Specialist",
+  },
+];
