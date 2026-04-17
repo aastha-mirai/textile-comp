b9b0917b7467422bfc8ca939a8ba97e3acf4a565
Durga <durga.jaiswal@miraiedge.com>
2026-04-17 16:04:50 +0530

    Fix why Choose us section

--- src/containers/WhyChooseUs/WhyChooseUs.component.tsx
+++ src/containers/WhyChooseUs/WhyChooseUs.component.tsx
@@ -1,5 +1,5 @@
 import { FeatureStat, Typography } from "@components";
-import { featureStatsCard } from "@mockData";
+import { whyChooseUsStat } from "@mockData";
 import bgImage from "@assets/imgs/why-choose-us-section-bg-image.webp";
 import { useScreenSize } from "@utils/useScreenSize";
 
@@ -7,14 +7,14 @@ const WhyChooseUs = () => {
   const { isMobile } = useScreenSize();
   return (
     <section
-      aria-label="Stats Section"
-      className="relative flex items-center justify-center flex-wrap py-12"
+      aria-label="Why Choose Us Section"
+      className="relative flex items-center justify-center flex-wrap md:pt-11 md:pb-19 py-8 md:px-2 px-5"
     >
       <img
         src={bgImage}
-        className=" z-n-level-2 w-full overflow-hidden absolute inset-0 w-full h-full object-cover"
+        className="z-n-level-2 overflow-hidden absolute inset-0 w-full h-full object-cover"
       />
-      <div className="flex flex-col items-center justify-center gap-8">
+      <div className="flex flex-col items-center justify-center gap-10 md:gap-16">
         {/* title */}
         <Typography
           color="white"
@@ -25,13 +25,15 @@ const WhyChooseUs = () => {
         />
         {/* content */}
         <div className="flex flex-wrap justify-center gap-12 w-full">
-          {featureStatsCard.map((card, index) => (
+          {whyChooseUsStat.map((card, index) => (
             <div key={card.id} className="flex items-center">
-              <FeatureStat {...card} />
+              <div className="lg:max-w-64">
+                <FeatureStat {...card} />
+              </div>
 
               {/* Divider except last item */}
-              {index !== featureStatsCard.length - 1 && (
-                <div className="xl:w-[2px] xl:h-12 xl:bg-darkGray xl:mx-12" />
+              {index !== whyChooseUsStat.length - 1 && (
+                <div className="xl:w-[2px] xl:h-12 xl:bg-white xl:mx-12" />
               )}
             </div>
           ))}
