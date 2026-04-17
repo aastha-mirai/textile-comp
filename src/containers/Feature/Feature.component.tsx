398b2c58e50836e5f581e1e65332893db05c9cbe
Durga <durga.jaiswal@miraiedge.com>
2026-04-17 19:12:17 +0530

    fix padding and gap issue

--- src/containers/Feature/Feature.component.tsx
+++ src/containers/Feature/Feature.component.tsx
@@ -5,11 +5,15 @@ const Feature = () => {
   return (
     <section
       aria-label="Stats Section"
-      className="h-auto flex bg-theme-gradient items-center justify-center gap-6 sm:gap-8 xl:gap-0 py-6 px-8 flex-wrap"
+      className="h-auto flex bg-theme-gradient items-center justify-center gap-2 px-1 xl:gap-0 py-6 flex-wrap"
     >
       {featureStatsCard.map((card, index) => (
         <div key={card.id} className="flex items-center">
-          <FeatureStat {...card} />
+          <FeatureStat
+            {...card}
+            textVariant="headline"
+            textVariantForMobile="body"
+          />
 
           {/* Divider except last item */}
           {index !== featureStatsCard.length - 1 && (
